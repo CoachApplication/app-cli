47396c1615dd4b8c71d90f127f71773b3e2202df
james.nesbitt@wunder.io <james.nesbitt@wunder.io>
2017-03-24 19:14:54 +0200

    start some more testing

---
+++ coach/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	cli_api "github.com/james-nesbitt/coach-cli/api"
+)
+
+func Test_Main(t *testing.T) {
+	ctx := context.Background()
+
+	workingDir, _ = os.Getwd()
+	settings := MakeLocalAPISettings(workingDir, ctx)
+
+	// Discover the current User (paths for the user like ~ and ~/.config/wundertools)
+	DiscoverUser(&settings)
+
+	// Discover for the project
+	DiscoverProject(&settings)
+
+	// if we have an environment set, then discover it.
+	if environment != "" {
+		DiscoverEnvironment(&settings, environment)
+	}
+
+	/**
+	 * The settings object has been created, now create an API,
+	 * and use it to get a list of operations, which we will
+	 * convert to CLI commands.
+	 */
+
+	// Build a local Application implementation from the settings
+	local, _ := cli_api.MakeLocalApp(settings)
+
+	// Get a list of operations from the API
+	localOps := local.Operations()
+
+	t.Error(localOps)
+}
